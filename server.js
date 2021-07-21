0d34180a19f68bb9d87c5f4b1f49248569c8ac98
philly <ph.stewart@icloud.com>
2021-07-21 11:04:40 -0400

    added server

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const hbs = exphbs.create({});
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 // Set Handlebars as the default template engine.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/dish-routes'));
+
 
 // Starts the server to begin listening
 app.listen(PORT, () => {
